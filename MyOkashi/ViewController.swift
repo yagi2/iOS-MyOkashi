34847289149e89acfe8e952d52bafee8cc42aec5
Itsuki Aoyagi <mail@yagi2.dev>
2021-06-08 03:53:49 +0900

    create main screen

--- MyOkashi/ViewController.swift
+++ MyOkashi/ViewController.swift
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
+    
+    @IBOutlet weak var searchText: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchText.delegate = self
+        searchText.placeholder = "お菓子の名前を入力してください"
     }
-
-
 }
-
