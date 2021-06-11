93ea560c2c930249cb9dc7439115b8f1fed471c0
Itsuki Aoyagi <mail@yagi2.dev>
2021-06-11 21:42:27 +0900

    fetch okashi list from API

--- MyOkashi/ViewController.swift
+++ MyOkashi/ViewController.swift
@@ -36,6 +36,35 @@ class ViewController: UIViewController, UISearchBarDelegate {
             return
         }
         
+        let request = URLRequest(url: requestUrl)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        
+        let task = session.dataTask(with: request, completionHandler: { data, response, error in
+            session.finishTasksAndInvalidate()
+            
+            do {
+                let decoder = JSONDecoder()
+                let json = try decoder.decode(ResultJson.self, from: data!)
+                
+                print(json)
+            } catch {
+                print("エラーです\(error)")
+            }
+        })
+        
+        task.resume()
+        
         print(requestUrl)
+    }
+    
+    struct ItemJson: Codable {
+        let name: String?
+        let maker: String?
+        let url: URL?
+        let image: URL?
+    }
+    
+    struct ResultJson: Codable {
+        let item: [ItemJson]?
     }
 }
