75fbff6c1651629e187e749394ed6a535db4c025
Itsuki Aoyagi <mail@yagi2.dev>
2021-06-11 21:49:15 +0900

    apply fetch result to table view

--- MyOkashi/ViewController.swift
+++ MyOkashi/ViewController.swift
@@ -11,12 +11,16 @@ class ViewController: UIViewController, UISearchBarDelegate {
     
     @IBOutlet weak var searchText: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
-
+    
+    private var okashiList: [(name: String, maker: String, link: URL, image: URL)] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         searchText.delegate = self
         searchText.placeholder = "お菓子の名前を入力してください"
+        
+        tableView.dataSource = self
     }
     
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
@@ -46,15 +50,23 @@ class ViewController: UIViewController, UISearchBarDelegate {
                 let decoder = JSONDecoder()
                 let json = try decoder.decode(ResultJson.self, from: data!)
                 
-                print(json)
+                if let items = json.item {
+                    self.okashiList.removeAll()
+                    for item in items {
+                        if let name = item.name, let maker = item.maker, let link = item.url, let image = item.image {
+                            let okashi = (name, maker, link, image)
+                            self.okashiList.append(okashi)
+                        }
+                    }
+                    
+                    self.tableView.reloadData()
+                }
             } catch {
                 print("エラーです\(error)")
             }
         })
         
         task.resume()
-        
-        print(requestUrl)
     }
     
     struct ItemJson: Codable {
@@ -66,5 +78,22 @@ class ViewController: UIViewController, UISearchBarDelegate {
     
     struct ResultJson: Codable {
         let item: [ItemJson]?
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return okashiList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "okashiCell", for: indexPath)
+        cell.textLabel?.text = okashiList[indexPath.row].name
+        
+        if let imageData = try? Data(contentsOf: okashiList[indexPath.row].image) {
+            cell.imageView?.image = UIImage(data: imageData)
+        }
+        
+        return cell
     }
 }
